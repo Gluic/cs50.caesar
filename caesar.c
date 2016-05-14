1d2c304bd1b40d112b8c5e2237dbb6dc8ffd1560
Gluic <gluic.hp@gmail.com>
2016-05-14 04:55:51 -0400

    final

--- caesar.c
+++ caesar.c
@@ -1,6 +1,6 @@
 /**
 * Caesar
-* Get a key and strin to code with caesar code
+* Get a key and string to code with caesar code
 * Print coded string
 */
 #include <stdio.h>
@@ -11,22 +11,34 @@
 
 int main (int argc, string argv [])
 {
+    // check if arguments more than 2 or 0
     if (argv[2] != NULL || argv[1] == NULL)
+    {
+        printf ("More than 1 or lack of arguments!\n");
         return 1;
+    }
+    // transform argument to int
     int k = atoi(argv[1]);
-    
+    k = k % 26;
+    // get string to code  
     char x;
     string text = GetString();
-    
+    // coding
     for (int i = 0, n = strlen(text); i < n; i++)
     {
+        // check if character is letter
         if (isalpha(text[i]))
         {
             x = text[i] + k;
-            x = x + '0';
-            printf ("%c",x);
+            if (isalpha(x))
+                printf ("%c",x);
+            else 
+            {   
+                x = x - 26;
+                printf ("%c",x);
+            } 
         }
-        
+        // dont code non letters
         if (isalpha(text[i]) == false)
         {
             printf ("%c",text[i]);
